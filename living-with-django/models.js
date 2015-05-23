e32b242fbbfe597bb02edb6db427b89be674c474
astex <0astex@gmail.com>
2015-05-23 15:24:57 -0400

    Add fetchSrc method to entry model.

--- living-with-django/models.js
+++ living-with-django/models.js
@@ -1,7 +1,14 @@
-define(['backbone'], function(B) {
+define(['backbone', 'marked'], function(B, marked) {
   var M = {};
 
-  M.Entry = B.Model.extend({});
+  M.Entry = B.Model.extend({
+    fetchSrc: function(cbs) {
+      require(['text!' + this.get('src')], function(src) {
+        this.set('content', marked(src));
+        _.finish(cbs);
+      });
+    }
+  });
   M.Entries = B.Collection.extend({
     model: M.Entry,
     url: 'entries/data.json'
