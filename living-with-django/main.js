c5489228d109e3e908832dcca5535d2aa6ab1908
astex <0astex@gmail.com>
2015-05-23 12:36:48 -0400

    Add require-css.

--- living-with-django/main.js
+++ living-with-django/main.js
@@ -3,6 +3,7 @@ window.blog = {DEUBUG: true};
 require.config({
   paths: {
     text: 'lib/require-text',
+    css: 'lib/require-css',
     jquery: 'lib/jquery',
     underscore: 'lib/underscore',
     backbone: 'lib/backbone'
