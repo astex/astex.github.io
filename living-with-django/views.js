2a22411cd2bd8401207eb3496becf0fb6b2dfa1b
astex <0astex@gmail.com>
2015-05-23 12:51:15 -0400

    Add main view.

--- living-with-django/views.js
+++ living-with-django/views.js
@@ -1,14 +1,17 @@
 define(
   [
     'underscore', 'backbone',
-    'models'
-  ], function(_, B, M) {
+    'models',
+    'text!templates/main.utpl'
+  ], function(_, B, M, t_main) {
     var V = {};
 
     V.Base = Backbone.View.extend({
       initialize: function() { this.render(); },
       render: function() { this.$el.html(this.template({model: this.model})); }
     });
+
+    V.Main = V.Base.extend({template: _.template(t_main)});
 
     return V;
   }
