9aabf9edb2d3e7e0bb8d786fde9386af9c2758c7
astex <0astex@gmail.com>
2015-05-23 12:34:22 -0400

    Add require-text.

--- living-with-django/main.js
+++ living-with-django/main.js
@@ -2,6 +2,7 @@ window.blog = {DEUBUG: true};
 
 require.config({
   paths: {
+    text: 'lib/require-text',
     jquery: 'lib/jquery',
     underscore: 'lib/underscore',
     backbone: 'lib/backbone'
